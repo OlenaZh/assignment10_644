3f306f88e01da9a65f487fb8a874816670e28aaa
OlenaZh <alyona.mesh@gmail.com>
2022-05-04 15:37:17 -0700

    math-done

--- js/math.js
+++ js/math.js
@@ -1,9 +1,25 @@
 //STEP 1
+/*let num = prompt('Please enter a number')
+console.log(Math.abs(num))*/
 
 //STEP 2
+/*let num = prompt('Please enter a floating point value (decimal).')
+console.log(Math.ceil(num))*/
 
 //STEP 3
+/*let num = prompt('Please enter a floating point value (decimal).')
+console.log(Math.floor(num))*/
 
 //STEP 4
+/*let nums = prompt("Please enter 5 numbers.");
+let num1 = (nums.charAt(0))
+let num2 = (nums.charAt(2))
+let num3 = (nums.charAt(4))
+let num4 = (nums.charAt(6))
+let num5 = (nums.charAt(8))
+console.log('The largest number is: ' + Math.max(num1, num2, num3, num4, num5));
+console.log('The smallest number is: ' + Math.min(num1, num2, num3, num4, num5));*/
 
 //STEP 5
+/*let num = prompt("Please enter a number");
+console.log(Math.sqrt(num));*/
