a74640bfaed0c8dfa64a3f06f70a04fe6de3e0db
OlenaZh <alyona.mesh@gmail.com>
2022-05-04 17:26:10 -0700

    dates - done

--- js/dates.js
+++ js/dates.js
@@ -1,10 +1,125 @@
 //STEP 1
-
-
+/*let myDate = new Date();
+ myDate.setMonth(4);
+ switch (myDate.getMonth()) {
+   case 0:
+     console.log('Jan has ' + 31 + ' days');
+     break;
+   case 1:
+     console.log('Feb has ' + 28 + ' days');
+     break;
+   case 2:
+     console.log('Mar has ' + 31 + ' days');
+     break;
+   case 3:
+     console.log('Apr has ' + 30 + ' days');
+     break;
+   case 4:
+     console.log('May has ' + 31 + ' days');
+     break;
+   case 5:
+     console.log('Jun has ' + 30 + ' days');
+     break;
+  case 6:
+    console.log('Jul has ' + 31 + ' days');
+     break;
+   case 7:
+     console.log('Aug has ' + 31 + ' days');
+     break;
+  case 8:
+    console.log('Sep has ' + 30 + ' days');
+     break;
+   case 9:
+     console.log('Oct has ' + 31 + ' days');
+     break;
+   case 10:
+    console.log('Nov has ' + 30 + ' days');
+     break;
+  case 11:
+     console.log('Dec has ' + 31 + ' days');
+     break;
+}
+*/
 //STEP 2
-
+/*let myDate = new Date();
+ myDate.setMonth(4);
+ switch (myDate.getMonth()) {
+   case 0:
+     console.log('Janurary');
+     break;
+   case 1:
+     console.log('Febuary');
+     break;
+   case 2:
+     console.log('March');
+     break;
+   case 3:
+     console.log('April');
+     break;
+   case 4:
+     console.log('May');
+     break;
+   case 5:
+     console.log('June');
+     break;
+   case 6:
+     console.log('July');
+     break;
+   case 7:
+     console.log('August');
+     break;
+   case 8:
+     console.log('Septemper');
+     break;
+   case 9:
+     console.log('October');
+     break;
+   case 10:
+     console.log('November');
+     break;
+   case 11:
+    console.log('December');
+     break;
+ }
+*/
 //STEP 3
-
+/*let myDate = new Date();
+ if (myDate.getDay() === 6 || myDate.getDay() === 0) {
+   console.log('It IS a weekend.');
+ } else {
+   console.log('It is NOT a weekend.');
+ }
+ */
 //STEP 4
+/*let myDate = new Date();
+let day = '';
+ myDate.setDate(myDate.getDate() -1);
 
+ switch (myDate.getDay()) {
+ case 0:
+   day = 'Sunday';
+   break;
+ case 1:
+   day = 'Monday';
+   break;
+ case 2:
+   day = 'Tuesday';
+   break;
+ case 3:
+   day = 'Wednesday';
+   break;
+ case 4:
+   day = 'Thursday';
+   break;
+ case 5:
+   day = 'Friday';
+   break;
+ case 6:
+   day = 'Saturday';
+   break;
+ }
+ console.log(day);
+ */
 //STEP 5
+/*let myDate = new Date();
+console.log(myDate.toDateString().slice(0, 1));*/
